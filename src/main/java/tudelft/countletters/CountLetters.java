86b75694c4301e0ceb529f93459a5dd68e17b24e
PBM <pbmalecki@wp.pl>
2018-11-22 22:25:06 +0100

    Implemented count letters class tests with full conditional coverage

--- src/main/java/tudelft/countletters/CountLetters.java
+++ src/main/java/tudelft/countletters/CountLetters.java
@@ -1,12 +1,13 @@
 package tudelft.countletters;
 
-public class CountLetters {
+class CountLetters {
 
-    public int count(String str) {
+    int count(String str) {
         int words = 0;
         char last = ' ';
-        for(int i = 0; i < str.length(); i++) {
-            if(!Character.isLetter(str.charAt(i)) &&
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isLetter(str.charAt(i)) &&
                     (last == 'r' || last == 's')) {
                 words++;
             }
@@ -14,7 +15,7 @@ public class CountLetters {
             last = str.charAt(i);
         }
 
-        if(last == 'x' || last == 's')
+        if (last == 'r' || last == 's')
             words++;
 
         return words;
--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -1,20 +1,50 @@
 package tudelft.countletters;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class CountLettersTest {
+class CountLettersTest {
+    private CountLetters countLetters;
+
+    @BeforeEach
+    void initialize() {
+        countLetters = new CountLetters();
+    }
 
     @Test
-    public void multipleMatchingWords() {
-        int words = new CountLetters().count("cats|dogs");
+    void multipleMatchingWords() {
+        int words = countLetters.count("cats|dogs");
         Assertions.assertEquals(2, words);
     }
 
     @Test
-    public void lastWordDoesNotMatch() {
-        int words = new CountLetters().count("cats|dog");
+    void firstWordEndsWithR() {
+        int words = countLetters.count("bear|dog");
         Assertions.assertEquals(1, words);
     }
 
+    @Test
+    void lastWordEndsWithR() {
+        int words = countLetters.count("cat|bear");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    void firstWordDoesNotMatch() {
+        int words = countLetters.count("cat|dogs");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    void lastWordDoesNotMatch() {
+        int words = countLetters.count("cats|dog");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    void noMatchingWords() {
+        int words = countLetters.count("cat|dog");
+        Assertions.assertEquals(0, words);
+    }
 }
